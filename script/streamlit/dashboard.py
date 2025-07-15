0a1313839147dc3dc01ffcc3f52272a72f271193
nkash <n.kashefipour@gmail.com>
2025-07-15 15:32:26 +0200

    test negative

--- script/streamlit/dashboard.py
+++ script/streamlit/dashboard.py
@@ -53,7 +53,7 @@ title = st.text_input(
 search_results = st.empty()
 
 st.subheader("Filter results")
-query = "SELECT B.title, S.phrase, S.polarity FROM books as B, reviews as R, sentiments as S WHERE B.asin = R.asin AND R.id = S.review_id ORDER BY B.asin LIMIT 1000"
+query = "SELECT B.title, S.phrase, S.polarity FROM books as B, reviews as R, sentiments as S WHERE S.polarity = 'negative' AND B.asin = R.asin AND R.id = S.review_id ORDER BY B.asin LIMIT 1000"
 filtered = get_data(query=query)
 st.dataframe(filtered)
 # # Kategorie-Filter
