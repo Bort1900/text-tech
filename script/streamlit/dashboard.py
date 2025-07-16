e46c202b62fd7efe6bb1560de69d7f23efdf5f2e
nkash <n.kashefipour@gmail.com>
2025-07-16 15:46:17 +0200

    first try filter

--- script/streamlit/dashboard.py
+++ script/streamlit/dashboard.py
@@ -5,6 +5,7 @@ import os
 from functools import partial
 import psycopg2
 
+
 st.title("Book Reviewscope - Amazon Reviews")
 
 
@@ -27,11 +28,7 @@ def get_data(query="SELECT * FROM books;", search_params=()):
     return df
 
 
-# Searching for books in the database to get asin
-st.subheader("Book Search")
-
-
-# TODO Author search, full text search
+# TODO Author search, full text search, minimizable
 def book_search(key):
     """
     returns the results for books as specified in the object value of the key object
@@ -44,21 +41,45 @@ def book_search(key):
         st.dataframe(books)
 
 
+def run_query():
+    """
+    collects all the filters and creates a query which is then applied to the dataframe
+    """
+    query_conditions = ""
+    params = []
+
+    if asin_choice:
+        st.write("filter by asin: ", asin_choice)
+        query_conditions += "AND B.asin=%s "
+        params.append(asin_choice)
+
+    complete_query = "SELECT B.title, B.genre, R.rating, R.summary, S.phrase, S.polarity, B.price, FROM books as B, reviews as R, sentiments as S WHERE B.asin = R.asin AND R.id = S.review_id {}ORDER BY B.asin LIMIT 1000"
+    filtered = get_data(query=complete_query, search_params=params)
+    with filtered_results:
+        st.dataframe(filtered)
+
+
+# Filter database with various parameters
+st.subheader("Filter results")
+
+# Filters
+asin_choice = st.text_input("asin", on_change=run_query)
+
+
+filtered_results = st.container()
+
+
+# Searching for books in the database to get asin
+st.subheader("Book Search")
+
 title = st.text_input(
     "Search for book title",
     placeholder="Lord of the Rings",
-    key="book_search",
+    key=ELEMENT_KEYS["book_search"],
     value="Lord",
-    on_change=partial(book_search, "book_search"),
+    on_change=partial(book_search, ELEMENT_KEYS["book_search"]),
 )
 search_results = st.container()
-
-st.subheader("Filter results")
-filtered_results = st.container()
-query = "SELECT B.title, S.phrase, S.polarity FROM books as B, reviews as R, sentiments as S WHERE S.polarity = 'negative' AND B.asin = R.asin AND R.id = S.review_id ORDER BY B.asin LIMIT 1000"
-filtered = get_data(query=query)
-with filtered_results:
-    st.dataframe(filtered)
 # # Kategorie-Filter
 # kategorien = df["kategorie"].dropna().unique()
 # kategorie_filter = st.multiselect("Kategorie auswählen", kategorien, default=kategorien)
