2671fede1b6f6b6c07d76a11c9ce990632f72777
nkash <n.kashefipour@gmail.com>
2025-07-17 15:02:41 +0200

    refactor without callbacks

--- script/streamlit/dashboard.py
+++ script/streamlit/dashboard.py
@@ -5,9 +5,6 @@ import os
 from functools import partial
 import psycopg2
 
-ELEMENT_KEYS = {"asin": "asin", "book_search": "book"}
-st.title("Book Reviewscope - Amazon Reviews")
-
 
 @st.cache_data
 def get_data(query="SELECT * FROM books;", search_params=()):
@@ -28,60 +25,46 @@ def get_data(query="SELECT * FROM books;", search_params=()):
     return df
 
 
-# TODO Author search, full text search, minimizable
-def book_search(key):
-    """
-    returns the results for books as specified in the object value of the key object
-    """
-    title = st.session_state[key]
-    query = f"SELECT asin, title FROM books WHERE title LIKE %s ORDER BY asin"
-    params = (f"%{title}%",)
-    books = get_data(query=query, search_params=params)
-    with search_results:
-        st.write("searching for " + title)
-        st.dataframe(books)
-
-
-def run_query():
-    """
-    collects all the filters and creates a query which is then applied to the dataframe
-    """
-    query_conditions = ""
-    params = []
-    asin = st.session_state[ELEMENT_KEYS["asin"]]
-    if asin:
-        st.write("filtering by asin: ", asin)
-        query_conditions += "AND B.asin=%s "
-        params.append(asin)
-
-    complete_query = f"SELECT B.title, B.genre, R.rating, R.summary, S.phrase, S.polarity, B.price FROM books as B, reviews as R, sentiments as S WHERE B.asin = R.asin AND R.id = S.review_id {query_conditions}ORDER BY B.asin LIMIT 1000"
-    filtered = get_data(query=complete_query, search_params=params)
-    with filtered_results:
-        st.write(complete_query, params)
-        st.dataframe(filtered)
-
-
+st.title("Book Reviewscope - Amazon Reviews")
 # Filter database with various parameters
 st.subheader("Filter results")
-
 # Filters
-asin_choice = st.text_input("asin", key=ELEMENT_KEYS["asin"], on_change=run_query)
-
+asin_choice = st.text_input("asin", key=ELEMENT_KEYS["asin"])
 
 filtered_results = st.container()
-
-
 # Searching for books in the database to get asin
 st.subheader("Book Search")
 
-title = st.text_input(
+title_search = st.text_input(
     "Search for book title",
     placeholder="Lord of the Rings",
-    key=ELEMENT_KEYS["book_search"],
-    value="Lord",
-    on_change=partial(book_search, ELEMENT_KEYS["book_search"]),
 )
 search_results = st.container()
+
+# collect all the filters and create a query which is then applied to the dataframe
+
+query_conditions = ""
+params = []
+if asin_choice:
+    query_conditions += "AND B.asin=%s "
+    params.append(asin_choice)
+
+complete_query = f"SELECT B.title, B.genre, R.rating, R.summary, S.phrase, S.polarity, B.price FROM books as B, reviews as R, sentiments as S WHERE B.asin = R.asin AND R.id = S.review_id {query_conditions}ORDER BY B.asin LIMIT 1000"
+filtered = get_data(query=complete_query, search_params=params)
+with filtered_results:
+    st.write(complete_query, params)
+    st.dataframe(filtered)
+
+# TODO Author search, full text search, minimizable
+# search for books and return the asin
+if title_search:
+    query = f"SELECT asin, title FROM books WHERE title LIKE %s ORDER BY asin"
+    params = (f"%{title_search}%",)
+    books = get_data(query=query, search_params=params)
+    with search_results:
+        st.dataframe(books)
+
+
 # # Kategorie-Filter
 # kategorien = df["kategorie"].dropna().unique()
 # kategorie_filter = st.multiselect("Kategorie auswählen", kategorien, default=kategorien)
